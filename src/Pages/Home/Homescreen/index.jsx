71f1b4861dd92e6a10acc1c6b0ef5455314766d0
saroj poudel <paudyalsaroj3@gmail.com>
2024-03-09 19:15:49 +0545

    Add files via upload

---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,14 @@
+
+// import Footer from "../Footer";
+
+
+import DataTabelVariable from "../array";
+export default function Home() {
+  return (
+    <>
+      <DataTabelVariable />
+      {/* <Footer /> */}
+     
+    </>
+  );
+}
